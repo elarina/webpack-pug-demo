0e393025d30725f1b0640f079dc71212ce02eeab
Elena Larina <elena.larina.1@gmail.com>
2021-04-07 08:54:42 +0300

    Commit whole project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    'index': './src/pages/index/index.pug',
+    'blog': './src/pages/blog/blog.pug'
+  },
+  output: {
+    filename: './js/[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    filename: 'index.html',
+    chunks: ['index'],
+    template: './src/pages/index/index.pug'
+    }),
+    new HtmlWebpackPlugin({
+    filename: 'blog.html',
+    chunks: ['blog'],
+    template: './src/pages/blog/blog.pug'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/i,
+        use: {
+          loader: 'pug-loader',
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+    ],
+  }
+};
